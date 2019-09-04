ed9ffdeb444d45f5143bd01882b8b6e8f757e54e
haider <haider.shah@hotmail.com>
2019-09-04 04:12:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,3 @@
 def nyc_pigeon_organizer(data)
- hash={}
- data.each do |colors_gender_lives,values|
-   values.each do |value,array|
-     array.each do |name|
-       if hash[name]== nil 
-         hash[name]={}
-         hash[name][colors_gender_lives]=[]
-       else
-          hash[name][colors_gender_lives]=[]
-        end
-      end
-    end
-    hash.each do |name,values|
-      values.each do |hashvalue,array|
-        data.each do |colors_gender_lives, values|
-          values.each do |value,array|
-            array.each do |element|
-              if element==name&&hashvalue==colors_gender_lives
-                
-                hash[name][hashvalue]<< value.to_s 
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-  hash
+ 
 end
